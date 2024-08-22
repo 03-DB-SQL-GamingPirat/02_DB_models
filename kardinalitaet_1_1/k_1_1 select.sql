f50e0f9b472ca1b9908da5bdb114ca1cd82556c9
Michael <GamingPirat@gmail.com>
2024-08-22 13:04:14 +0200

    1_1 / INNER JOIN

---
+++ kardinalitaet_1_1/k_1_1 select.sql
@@ -0,0 +1,39 @@
+-- SELECTS
+
+
+-- Einzeltabellen
+SELECT * FROM design.cats;
+SELECT * FROM design.servants;
+
+-- Kreuzprodukt (Kartesisches Produkt)
+SELECT * FROM design.cats JOIN design.servants;
+
+--Inner Join / Gesamte Tabelle
+SELECT
+    *
+    FROM design.cats INNER JOIN design.servants
+    ON
+        design.cats.id = design.servants.cats_id
+        ;
+
+-- Inner Join 2 / (Wer dient wem?)
+-- Wer dient Grizabella?
+-- Wem dient Sven?
+
+SELECT
+    cat_name AS Katze,
+    servant_name AS Diener
+FROM design.cats INNER JOIN design.servants
+ON design.cats.id = design.servants.cats_id
+-- Filtern mit WHERE
+WHERE cat_name = "Grizabella"
+;
+
+SELECT
+    cat_name AS Katze,
+    servant_name AS Diener
+FROM design.cats INNER JOIN design.servants
+ON design.cats.id = design.servants.cats_id
+-- Filtern mit WHERE
+WHERE servant_name = "Sven"
+;
